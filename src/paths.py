a3ccc2d939f8bc33fe38736eb323dfe50ea9f3be
Ueli <ueli.schilt@hslu.ch>
2025-11-25 08:22:06 +0100

    cleaned paths.py

--- src/paths.py
+++ src/paths.py
@@ -22,7 +22,6 @@ else:
 simulation_data_dir = '../data/master_data/simulation_data/'
 
 master_file = 'df_master_sim.feather'
-# meta_file = 'df_meta_2.feather'
 meta_file = 'meta_file_2.feather'
 
 
@@ -32,22 +31,8 @@ profiles_file = 'simulation_profiles_file.feather'
 # -----------
     
 # Directory paths:
-# master_data_dir = 'C:/Users/Somesh/OneDrive - Hochschule Luzern/Dokumente/Assessment/Biomasse/50_Code/district_energy_model/data/master_data/'
-# master_data_dir = 'C:/Users/UeliSchilt/Hochschule Luzern/CC TES - 1122442_EDGE/Modelling/Masters/'
 master_data_dir = '../data/master_data/'
 com_data_dir = '../data/community_data/'
-
-# results_path = 'C:/Users/UeliSchilt/Hochschule Luzern/CC TES - EDGE/Modelling/Results/District_Energy_Model/Multi_obj_opt/Allschwil/v20240312_with_tes_5_weights'
-# results_path = 'tmp_results_for_testing'
-# results_path = 'tmp_results_for_testing/TEST_BALANCE/ISSUE_20240408_electricity_balance'
-# results_path = 'C:/Users/UeliSchilt/Hochschule Luzern/CC TES - EDGE/Modelling/Results/District_Energy_Model/Multi_obj_opt/Sirnach'
-# results_path = 'C:/Users/UeliSchilt/Hochschule Luzern/CC TES - EDGE/Modelling/Results/District_Energy_Model/Multi_obj_opt/0_tmp_new_results'
-
-# Filenames:
-# master_file = 'Schweiz.csv'
-# master_file = '/simulation_data/df_master_sim.feather'
-# meta_file = 'meta.csv' # 'meta_0623.csv'
-# meta_file = '/simulation_data/meta_file_2.feather'
 
 # -----------------------------------------------------------------------------
 # dem.py:
@@ -57,9 +42,7 @@ input_files_dir = '../config/input_files'
 
 # Input data directories:
 weather_data_dir = path_strt1 + '/data/heat_demand/weather_data/' # location of meteostat files
-
 weather_data_delta_method_dir = path_strt1 + '/data/weather_data/com_files/'
-
 dhw_profile_dir = path_strt1 + '/data/heat_demand/'
 pv_data_dir = path_strt1 + '/data/pv_data/pv_input_file/' # location of pv data
 energy_mix_CH_dir = path_strt1 + '/data/electricity_mix_national/' # location of energy mix files
@@ -67,7 +50,6 @@ electricity_profile_dir = path_strt1 + '/data/electricity_demand/' # location of
 biomass_data_dir = path_strt1 + '/data/biomass_data/'
 wind_power_data_dir = path_strt1 + '/data/tech_wind_power/' # location of wind power data (e.g. installed capacities per municipality)
 wind_power_profiles_dir = path_strt1 + '/data/tech_wind_power/profiles/' # location of wind power hourly profile files
-# electr_prod_plant_dir = path_strt1 + '/data/electricity_production_plant/'
 ev_profiles_dir = path_strt1 + '/data/electricity_demand/ev_profiles/' # location of electric vehicle (ev) charging profiles
 
 # Input data files:
@@ -80,21 +62,12 @@ electricity_demand_file_industry = path_strt1 + '/data/electricity_demand/electr
 pv_data_meta_file = 'pv_data_meta.csv' # csv file containing meta data about pv profile files
 electricity_profile_file = 'load_profiles.csv' # csv-file containing load profile from smart meter data
 electricity_profile_industry_file = 'cantonal_industryprofiles.feather' # csv-file containing load profile from smart meter data
-# electricity_profile_file = 'Buildings_A_B_power_load_profile.csv' # csv-file containing load profile from smart meter data
-# electr_prod_plant_file = 'ElectricityProductionPlant.csv' # electricity production plants in Switzerland (from UVEK)
-# wind_power_cap_file = 'p_installed_kW_wind_power.csv' # installed wind power capacity [kW] per municipality
 wind_power_cap_file = 'p_installed_kW_wind_power.feather' # installed wind power capacity [kW] per municipality
 wind_power_national_profile_file = 'v_e_wp_national_installed_kWh.csv' # Hourly profile of national wind power generation [kWh]
 dhw_profile_file = 'DHW_Profile.feather'
 hydro_profile_file = path_strt1 + '/data/electricity_production_plant/HydroProfiles/Hydro_Profiles.feather'
-# electr_prod_plant_file = 'ElectricityProductionPlant_wind.csv' # electricity production plants in Switzerland (from UVEK); TMP: only for wind, with correct munic names
 ev_profile_cp_file = 'profile_CP_y4.feather' # hourly charging load [kW]
 ev_profile_fe_file = 'profile_FE_y4.feather' # daily flexible energy [kWh]
 ev_profile_pd_file = 'profile_PD_y4.feather' # hourly upper power bound [kW]
 ev_profile_pu_file = 'profile_PU_y4.feather' # hourly lower power bound [kW]
 ev_munic_name_nr_file = 'ev_munic_name_nr.feather' # municipalities and BFS numnbers for ev data
-
-
-# Other files:
-# municipality_names_file = r'C:\Users\UeliSchilt\Hochschule Luzern\CC TES - EDGE\Resources\municipality_names.xlsx'
-
